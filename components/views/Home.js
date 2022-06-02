e90ba0669b3a3ee4020054d47788462bd6aea12e
Casey Selby <caseyjselby@gmail.com>
2022-06-01 21:55:38 -0700

    a

--- components/views/Home.js
+++ components/views/Home.js
@@ -2,13 +2,11 @@ import html from "html-literal";
 
 export default state => html`
   <section id="jumbotron">
-    <div id="mainContent">
-      <h2>The Date Deck</h2>
-      <p id="description">
-        Whether you're looking for adventurous date ideas, group dates, date
-        ideas by season or at-home date ideas, we've got you covered.
-      </p>
-      <a href="index.html">Generate a Date</a>
-    </div>
+    <h2>The Date Deck</h2>
+    <p id="description">
+      Whether you're looking for adventurous date ideas, group dates, date ideas
+      by season or at-home date ideas, we've got you covered.
+    </p>
+    <a href="index.html">Generate a Date</a>
   </section>
 `;
