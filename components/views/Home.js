9bfa7f35083695430c79488f5dc337ca0114fa0c
Casey Selby <caseyjselby@gmail.com>
2022-06-01 22:08:06 -0700

    a

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,8 +1,10 @@
 import html from "html-literal";
+import sunsetPic from "../../assets/img/sunset.jpg";
 
 export default state => html`
   <section id="jumbotron">
     <h2>The Date Deck</h2>
+    <img src="${sunsetPic}" alt="Couple Watching the Sunset" />
     <p id="description">
       Whether you're looking for adventurous date ideas, group dates, date ideas
       by season or at-home date ideas, we've got you covered.
