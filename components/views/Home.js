6ec794f9f43e0ca0517499f9899d207fe2377f4a
Casey Selby <caseyjselby@gmail.com>
2022-06-01 22:47:23 -0700

    a

--- components/views/Home.js
+++ components/views/Home.js
@@ -6,11 +6,13 @@ export default state => html`
   <section id="jumbotron">
     <h2>The Date Deck</h2>
     <img src="${sunsetPic}" alt="Couple Watching the Sunset" />
-    <img id="ms" src="${milkshakePic}" alt="Cheers with milkshakes" />
     <p id="description">
       Whether you're looking for adventurous date ideas, group dates, date ideas
       by season or at-home date ideas, we've got you covered.
     </p>
     <a href="index.html">Generate a Date</a>
+  </section>
+  <section id="ms">
+    <img src="${milkshakePic}" alt="Cheers with milkshakes" />
   </section>
 `;
