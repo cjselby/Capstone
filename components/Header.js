bdb6013fb4058b1b648e385f1c8b9ce27d3807f7
Casey Selby <caseyjselby@gmail.com>
2022-06-01 23:12:07 -0700

    a

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,7 @@
 import html from "html-literal";
-import skyPic from "../../assets/img/skydive.jpg";
 
 export default state => html`
   <header>
     <h1>The Date Deck ${state.header}</h1>
-    <img src="${skyPic}" />
   </header>
 `;
