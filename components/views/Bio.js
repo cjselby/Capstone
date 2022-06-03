e3fa424ec01c169b3019dfece0734906dc2ac339
Casey Selby <caseyjselby@gmail.com>
2022-06-02 22:50:01 -0700

    a

--- components/views/Bio.js
+++ components/views/Bio.js
@@ -1,12 +1,14 @@
 import html from "html-literal";
 export default () => html`
   <section id="bio">
-    <h2>About Us</h2>
-    <p>
-      I created The Date Deck because I want inspire others to deepen their
-      connection with loved ones in new ways. I know life gets busy and
-      maintaining relationships is difficult. Instead of spending time planning
-      a date, The Date Deck will do it for you.
-    </p>
+    <div id="bioContent">
+      <h2>Mission</h2>
+      <p>
+        I created The Date Deck because I want inspire others to deepen their
+        connection with loved ones in new ways. I know life gets busy and
+        maintaining relationships is difficult. Instead of spending time
+        planning a date, The Date Deck will do it for you.
+      </p>
+    </div>
   </section>
 `;
