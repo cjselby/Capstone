679883b36de0238b32a236ffd914daaa9d95f7bf
Casey Selby <caseyjselby@gmail.com>
2022-06-02 22:20:00 -0700

    a

--- components/views/Home.js
+++ components/views/Home.js
@@ -10,6 +10,9 @@ export default state => html`
     <a href="index.html">Generate a Date</a>
   </section>
   <section id="secondSection">
-    <p id="secondP">Discover new ways to connect with your loved ones.</p>
+    <p id="secondP">Let us be your guide to making new memories.</p>
+  </section>
+  <section id="thirdSection">
+    <p id="thirdP">Discover new ways to connect with your loved ones.</p>
   </section>
 `;
