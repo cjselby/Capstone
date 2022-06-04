dfa69a84829d616b43fbdf9ec72511fd955af5c5
Casey Selby <caseyjselby@gmail.com>
2022-06-03 23:14:28 -0700

    a

--- components/views/Home.js
+++ components/views/Home.js
@@ -2,10 +2,12 @@ import html from "html-literal";
 
 export default state => html`
   <section id="jumbotron">
-    <h2>Not your average date night</h2>
     <div class="jumboImg">
-      Whether you're looking for adventurous date ideas, group dates, date ideas
-      by season or at-home date ideas, we've got you covered.
+      <h2>Not your average date night</h2>
+      <p>
+        Whether you're looking for adventurous date ideas, group dates, date
+        ideas by season or at-home date ideas, we've got you covered.
+      </p>
     </div>
     <a href="index.html">Generate a Date</a>
   </section>
