2e2d823acbc513bfa307ec99be716cd37ea3d8b7
Casey Selby <caseyjselby@gmail.com>
2022-06-06 15:54:19 -0700

    a

--- components/Footer.js
+++ components/Footer.js
@@ -5,6 +5,5 @@ let year = new Date().getFullYear();
 export default () => html`
   <footer>
     &copy; ${year} <a href="https://github.com/cjselby/Capstone">Casey Selby</a>
-    <a href="Form.js">Help Us Improve</a>
   </footer>
 `;
