02ed79b69b460c1097c563a10ffc90248e088d8a
Casey Selby <caseyjselby@gmail.com>
2022-06-07 15:45:35 -0700

    a

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ export default () => html`
     &copy; ${year} <a href="https://github.com/cjselby/Capstone">Casey Selby</a>
     <div class="review">
       <ul>
-        <li><a href="Form.js">Submit a Review</a></li>
+        <li><a href="./Form">Submit a Review</a></li>
       </ul>
     </div>
   </footer>
