3eb3a6405bacc1550ae28637b38216159638a15a
Casey Selby <caseyjselby@gmail.com>
2022-06-12 15:22:10 -0700

    a

--- components/views/All.js
+++ components/views/All.js
@@ -2,15 +2,18 @@ import html from "html-literal";
 export default () => html`
   <section id="allDates">
     <div id="content1">
-      <h2>Group Date Activities</h2>
+      <h2>Group Date Ideas</h2>
     </div>
     <div id="img1"></div>
     <div id="img2"></div>
     <div id="img3"></div>
     <div id="img4"></div>
     <div id="content2">
-      <h2>Music & Art Date Activities</h2>
+      <h2>Music & Art Date Ideas</h2>
     </div>
     <div id="img5"></div>
+    <div id="img6"></div>
+    <div id="img7"></div>
+    <div id="img8"></div>
   </section>
 `;
