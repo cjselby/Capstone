16cc54276ffd69139433f930596cdf541047a203
Casey Selby <caseyjselby@gmail.com>
2022-06-11 21:51:11 -0700

    a

--- components/views/Home.js
+++ components/views/Home.js
@@ -14,19 +14,21 @@ export default state => html`
       <a href="index.html">Explore all date categories</a>
     </div>
   </section>
-  <section class="categorySection">
-    <h2>Group Dates</h2>
-    <div id="secondImg">
-      <h3>Bowling</h3>
-    </div>
-    <div id="thirdImg">
-      <h3>Softball</h3>
-    </div>
-  </section>
-  <section class="categorySection">
-    <h2>Outdoor Dates</h2>
-    <div id="hikeImg">
-      <h3>Hiking</h3>
-    </div>
-  </section>
+  <div id="wrapper">
+    <section class="categorySection">
+      <h2>Group Dates</h2>
+      <div id="secondImg">
+        <h3>Bowling</h3>
+      </div>
+      <div id="thirdImg">
+        <h3>Softball</h3>
+      </div>
+    </section>
+    <section class="categorySection">
+      <h2>Outdoor Dates</h2>
+      <div id="hikeImg">
+        <h3>Hiking</h3>
+      </div>
+    </section>
+  </div>
 `;
