37bce44118e462eba45d2d3465043a176907f380
Casey Selby <caseyjselby@gmail.com>
2022-06-11 17:30:10 -0700

    a

--- components/views/Home.js
+++ components/views/Home.js
@@ -11,10 +11,16 @@ export default state => html`
       <a href="index.html">Explore all date categories</a>
     </div>
   </section>
-  <section id="secondSection">
+  <section class="categorySection">
     <h2>Group Dates</h2>
     <div id="secondImg">
-      a
+      <h3>Bowling</h3>
+    </div>
+  </section>
+  <section class="categorySection">
+    <h2>Outdoor Dates</h2>
+    <div id="hikeImg">
+      <h3>Hiking</h3>
     </div>
   </section>
 `;
