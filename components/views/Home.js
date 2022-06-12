9fa3e80a80abec707d3d38ca4307c7dd2045a45f
Casey Selby <caseyjselby@gmail.com>
2022-06-11 19:30:37 -0700

    a

--- components/views/Home.js
+++ components/views/Home.js
@@ -15,23 +15,18 @@ export default state => html`
     </div>
   </section>
   <section class="categorySection">
-    <section id="groupSection">
-      <h2>Group Dates</h2>
-      <section id="secondImg">
-        <h3>Bowling</h3>
-        <img src="${bowlingPic}" alt="bowling alley" />
-      </section>
-      <div id="thirdImg">
-        <h3>Softball</h3>
-        <!-- <img src="${softballPic}" alt="group of people playing softball" /> -->
-      </div>
-    </section>
-    <section id="outdoorSection">
-      <h2>Outdoor Dates</h2>
-      <div id="hikeImg">
-        <h3>Hiking</h3>
-        <!-- <img src="${hikePic}" alt="couple hiking" /> -->
-      </div>
-    </section>
+    <h2>Group Dates</h2>
+    <div id="secondImg">
+      <h3>Bowling</h3>
+    </div>
+    <div id="thirdImg">
+      <h3>Softball</h3>
+    </div>
+  </section>
+  <section class="categorySection">
+    <h2>Outdoor Dates</h2>
+    <div id="hikeImg">
+      <h3>Hiking</h3>
+    </div>
   </section>
 `;
