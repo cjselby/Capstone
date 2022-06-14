977aee27a1552802719df5ec683329eae0822f5e
Casey Selby <caseyjselby@gmail.com>
2022-06-14 16:54:42 -0700

    update

--- components/views/Food.js
+++ components/views/Food.js
@@ -1,7 +1,7 @@
 import html from "html-literal";
 export default () => html`
   <section id="food">
-    <div id="foodContent">
+    <div id="dateIdea">
       <h2>Potluck style picnic</h2>
       <a href="Groups" button>Next<i class="fas fa-chevron-right"></i></a>
     </div>
--- components/views/Outdoors.js
+++ components/views/Outdoors.js
@@ -1,8 +1,8 @@
 import html from "html-literal";
 export default () => html`
   <section id="outdoors">
-    <div id="outdoorContent">
-      <h2 class="generateTitle">Go camping</h2>
+    <div id="dateIdea">
+      <h2>Go camping</h2>
       <a href="Food" button>Next<i class="fas fa-chevron-right"></i></a>
     </div>
   </section>
