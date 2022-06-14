31b48ee1054ed4c62b3d75c0221fb0579a231ae6
Casey Selby <caseyjselby@gmail.com>
2022-06-14 16:40:22 -0700

    update

--- components/views/Art.js
+++ components/views/Art.js
@@ -1,8 +1,8 @@
 import html from "html-literal";
 export default () => html`
   <section id="art">
-    <div id="artContent">
-      <h2 class="generateTitle">Take a pottery class</h2>
+    <div id="dateIdea">
+      <h2>Take a pottery class</h2>
       <a href="Outdoors" button>Next<i class="fas fa-chevron-right"></i></a>
     </div>
   </section>
--- components/views/Groups.js
+++ components/views/Groups.js
@@ -1,9 +1,8 @@
 import html from "html-literal";
 export default () => html`
   <section id="group">
-    <div id="groupContent">
+    <div id="dateIdea">
       <h2>Have a bonfire</h2>
-      <p>Pro tip: s'mores are always a good idea</p>
       <a href="Art" button>Next<i class="fas fa-chevron-right"></i></a>
     </div>
   </section>
