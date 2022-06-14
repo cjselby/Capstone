2ff492748276eead67a36b33360fabefe8b49ba1
Casey Selby <caseyjselby@gmail.com>
2022-06-14 14:10:56 -0700

    a

--- components/views/Groups.js
+++ components/views/Groups.js
@@ -2,7 +2,8 @@ import html from "html-literal";
 export default () => html`
   <section id="group">
     <div id="groupContent">
-      <h2>Group Date Ideas</h2>
+      <h2>Have a bonfire</h2>
+      <p>Pro tip: s'mores are always a good idea</p>
     </div>
   </section>
 `;
