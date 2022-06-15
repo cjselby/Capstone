2dd152ec508574597d190933558b00ab058a6438
Casey Selby <caseyjselby@gmail.com>
2022-06-14 19:19:14 -0700

    a

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,6 @@ import html from "html-literal";
 
 export default state => html`
   <header>
-    <h1>The Date Deck ${state.header}</h1>
+    <h1><a href="Home">The Date Deck ${state.header}</a></h1>
   </header>
 `;
