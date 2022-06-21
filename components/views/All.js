56a6d5cecd3d27a887c39e98f876b091a4054afd
Casey Selby <caseyjselby@gmail.com>
2022-06-20 21:54:10 -0700

    updated all

--- components/views/All.js
+++ components/views/All.js
@@ -1,28 +1,40 @@
 import html from "html-literal";
+import sailPic from "../../assets/img/sailing.jpg";
+import bonfirePic from "../../assets/img/groupbonfire.jpg";
+import pokerPic from "../../assets/img/poker.jpg";
+import fairPic from "../../assets/img/fair.jpg";
+import concertPic from "../../assets/img/concert.jpg";
+import paintPic from "../../assets/img/paint.jpg";
+import museumPic from "../../assets/img/museum.jpg";
+import potteryPic from "../../assets/img/pottery.jpg";
 import winePic from "../../assets/img/wine.jpg";
+import cookPic from "../../assets/img/cooking.jpg";
+import milkshakePic from "../../assets/img/milkshake.jpg";
+import picnicPic from "../../assets/img/picnic.jpg";
 
 export default () => html`
   <section id="allDates">
     <div id="content1">
       <h2>Group Dates</h2>
     </div>
-    <div id="img1"></div>
-    <div id="img2"></div>
-    <div id="img3"></div>
-    <div id="img4"></div>
+    <img id="img1" src="${sailPic}" alt="sailing" />
+    <p class="imgTxt">sailing</p>
+    <img id="img2" src="${bonfirePic}" alt="bonfire" />
+    <img id="img3" src="${pokerPic}" alt="poker" />
+    <img id="img4" src="${fairPic}" alt="fair" />
     <div id="content2">
       <h2>Music & Art Dates</h2>
     </div>
-    <div id="img5"></div>
-    <div id="img6"></div>
-    <div id="img7"></div>
-    <div id="img8"></div>
+    <img id="img5" src="${concertPic}" alt="concert" />
+    <img id="img6" src="${paintPic}" alt="painting" />
+    <img id="img7" src="${museumPic}" alt="museum" />
+    <img id="img8" src="${potteryPic}" alt="pottery" />
     <div id="content3">
       <h2>Dinner and Drink Dates</h2>
     </div>
     <img id="img9" src="${winePic}" alt="wine" />
-    <div id="img10"></div>
-    <div id="img11"></div>
-    <div id="img12"></div>
+    <img id="img10" src="${cookPic}" alt="cooking" />
+    <img id="img11" src="${milkshakePic}" alt="milkshake cheers" />
+    <img id="img12" src="${picnicPic}" alt="picnic" />
   </section>
 `;
--- components/views/Bio.js
+++ components/views/Bio.js
@@ -4,10 +4,10 @@ export default () => html`
     <div id="bioContent">
       <h2>Mission</h2>
       <p>
-        I created The Date Deck because I want inspire others to deepen their
-        connection with loved ones in new ways. I know life gets busy and
-        maintaining relationships is difficult. Instead of spending time
-        planning a date, The Date Deck will do it for you.
+        I created The Date Deck to inspire others to deepen their connection
+        with loved ones in new ways. I know life gets busy and maintaining
+        relationships is difficult. Instead of spending time planning a date,
+        The Date Deck will do it for you.
       </p>
     </div>
   </section>
