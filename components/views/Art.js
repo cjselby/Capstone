1ed8c50dc7c84cd4ae3e70861c73b23a1f3d1531
Casey Selby <caseyjselby@gmail.com>
2022-06-23 17:43:55 -0700

    update background

--- components/views/Art.js
+++ components/views/Art.js
@@ -3,7 +3,7 @@ export default () => html`
   <section id="art">
     <div id="dateIdea">
       <h2>Take a pottery class</h2>
-      <a href="Groups" button><i class="fas fa-chevron-left">Back</i></a>
+      <a href="Groups" button><i class="fas fa-chevron-left"></i>Back</a>
       <a href="Golf" button>Next<i class="fas fa-chevron-right"></i></a>
     </div>
   </section>
